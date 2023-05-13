7496e1f7bf36a369148ac846094a45819683a184
Nemanja Kozlina <nemanja.kozlina.21@singimail.rs>
2023-05-13 23:44:48 +0200

    Starter projekat

---
+++ demo1.fe/src/pages/users/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function UserPage() {
+    return (
+        <>
+            <h1>Users Page</h1>
+            <Link href={"/"}>Home page</Link>
+        </>
+    )
+}
